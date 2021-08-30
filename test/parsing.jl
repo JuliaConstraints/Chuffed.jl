4562b3aad31e0222c9dfd63cdb13d1fee0f682c4
Thibaut Cuvelier <dourouc05@gmail.com>
2021-08-30 03:28:50 +0200

    Explictly test Chuffed output for the four test cases.

--- test/parsing.jl
+++ test/parsing.jl
@@ -3,6 +3,21 @@
         out_string = Chuffed.run_chuffed(@__DIR__() * "/assets/basic.fzn")
         @test out_string == "x = 3;\r\n\r\n----------\r\n"
     end
+
+    @testset "one_solution.fzn" begin
+        out_string = Chuffed.run_chuffed(@__DIR__() * "/assets/one_solution.fzn")
+        @test out_string == "x = 10;\r\n\r\n----------\r\n==========\r\n"
+    end
+
+    @testset "several_solutions.fzn" begin
+        out_string = Chuffed.run_chuffed(["-a", @__DIR__() * "/assets/several_solutions.fzn"])
+        @test out_string == "xs = array1d(1..2, [2, 3]);\r\n\r\n----------\r\nxs = array1d(1..2, [1, 3]);\r\n\r\n----------\r\nxs = array1d(1..2, [1, 2]);\r\n\r\n----------\r\n==========\r\n"
+    end
+
+    @testset "puzzle.fzn" begin
+        out_string = Chuffed.run_chuffed(@__DIR__() * "/assets/puzzle.fzn")
+        @test out_string == "x = array2d(1..4, 1..4, [5, 1, 8, 8, 9, 3, 8, 6, 9, 7, 7, 8, 1, 7, 8, 9]);\r\n\r\n----------\r\n"
+    end
 end
 
 # TODO: move to CP.

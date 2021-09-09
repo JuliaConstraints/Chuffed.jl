dc470bd553e9c5b2bc93ae492f7e221b50e4958d
Thibaut Cuvelier <dourouc05@gmail.com>
2021-09-09 21:03:30 +0200

    Adapt to code moving to CP.

--- src/Chuffed.jl
+++ src/Chuffed.jl
@@ -1,24 +1,16 @@
 module Chuffed
 
-import MathOptInterface
 import ConstraintProgrammingExtensions
 using Chuffed_jll
 
-const MOI = MathOptInterface
-const MOIU = MOI.Utilities
-const CleverDicts = MOIU.CleverDicts
 const CP = ConstraintProgrammingExtensions
-
-module FZN
-include("FlatZincOptimizer.jl")
-end
 
 function run_chuffed(args)
     return String(read(`$(Chuffed_jll.fznchuffed()) $(args)`))
 end
 
 function Optimizer()
-    return FZN.Optimizer(Chuffed_jll.fznchuffed())
+    return CP.FlatZinc.Optimizer(Chuffed_jll.fznchuffed())
 end
 
 end # module
